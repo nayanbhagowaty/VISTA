491c3aa0252bbd4db1c2a7ee50bcfee85c397736
Nayan Bhagowaty <nayan.bhagowaty.2@consultant.volvo.com>
2024-05-31 09:52:19 +0200

    Named it BEAN - Beginner's Exploring AI Nuances

--- LlmChatBot.AppHost/Program.cs
+++ LlmChatBot.AppHost/Program.cs
@@ -1,12 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+//var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.LlmChatBot_ApiService>("apiservice");
 
 builder.AddProject<Projects.LlmChatBot_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(cache)
+    //.WithReference(cache)
     .WithReference(apiService);
 
 builder.Build().Run();
